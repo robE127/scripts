087f505341b60638b7d654ada94b424acfe4608f
Rob Edwards <roced3@gmail.com>
2016-05-17 16:35:15 -0400

    Made it start in the past and move to the present.

--- retentionTester.py
+++ retentionTester.py
@@ -1,3 +1,6 @@
+#
+
+
 #!/usr/bin/python
 
 import subprocess, calendar, time
@@ -16,14 +19,21 @@ PAUSE_KEY = ".backupPause"
 ZFS_AGENT_PATH = "homePool/home/agents/"
 SAFE_RETENTION = "240000:240000:240000:240000"
 START_BACKUP = "snapctl foregroundStart "
+OFFSITE_CONTROL_KEY = ".offsiteControl"
 
 # Prompt the user for the system to test with
-agent = raw_input("What existing agent do you want to test with? ")
+agent = raw_input("What existing agent do you want to test with:  ")
+testLength = raw_input("How many months of backups do you want to create: ")
 
 # Pause automatic backups for the agent
 backupPause = open(CONFIG_KEYS + agent + PAUSE_KEY, 'w')
 backupPause.write("1")
 backupPause.close()
+
+# Set offsite to never because I think going back in time is going to confuse speedsync.
+offsiteControl = open(CONFIG_KEYS + agent + OFFSITE_CONTROL_KEY, 'w')
+offsiteControl.write('{"interval":"-1","latestSnapshot":"0","latestOffsiteSnapshot":"0"}')
+offsiteControl.close()
 
 # Change the local retention settings to NEVER for ALL fields.
 retentionSettingsFile = open(CONFIG_KEYS + agent + RETENTION_KEY, 'w')
@@ -34,25 +44,28 @@ retentionSettingsFile.close()
 subprocess.call("chmod 000 /etc/ntp.conf", shell=True)
 subprocess.call("chmod 000 /etc/cron.d/datto-codebase-core", shell=True)
 
+# Get the current time, convert to EPOCH format, and start at fakeTime.
 realTime = calendar.timegm(time.gmtime())
-fakeTime = realTime
+fakeTime = realTime - (MONTH * int(testLength))
+startFakeTime = fakeTime
+subprocess.call("date -s @" + str(fakeTime), shell=True)
 
 # Run 3 backups a day for roughly 3 months
 for d in range(1,92):
 	for h in range(1,4):
 		# Start a backup
-		#print("Starting a backup at " + time.gmtime(fakeTime))
-		subprocess.call(START_BACKUP + agent, shell=True)
-		# Go back in time by h hours
-		fakeTime = fakeTime - HOUR
+		print("Starting a backup at " + str(time.gmtime(fakeTime)))
+		#subprocess.call(START_BACKUP + agent, shell=True)
+		# Go forward in time by h hours
+		fakeTime = fakeTime + HOUR
 		subprocess.call("date -s @" + str(fakeTime), shell=True)
-	# Go back in time by (DAY * d) days from actual time each iteration
-	subprocess.call("ntpdate ntp.dattobackup.com", shell=True)
-	realTime = calendar.timegm(time.gmtime())
-	fakeTime = realTime - (DAY * d)
+		time.sleep(3)
+	# Go forward in time by (DAY * d) days from startFakeTime
+	fakeTime = startFakeTime + (DAY * d)
 	subprocess.call("date -s @" + str(fakeTime), shell=True)
+	time.sleep(3)
 	
-# Return ntp config and time back to good state
+# Return ntp config and time back to normal state
 subprocess.call("ntpdate ntp.dattobackup.com", shell=True)
 subprocess.call("chmod 644 /etc/ntp.conf", shell=True)
 subprocess.call("chmod 644 /etc/cron.d/datto-codebase-core", shell=True)
