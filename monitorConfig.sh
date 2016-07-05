82ee682958d3b1bdf5b0d3d93c9271dcaeb61524
robE127 <roced3@gmail.com>
2016-07-05 16:56:55 -0400

    added handling for dirs with different file naming

--- monitorConfig.sh
+++ monitorConfig.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-keys=(maxBackups backupOffset useAdvancedAlerting cefActive disableScreenshots useRDPConnection autoDiffMerge defaultTP)
+globalKeys=(maxBackups backupOffset useAdvancedAlerting cefActive disableScreenshots useRDPConnection autoDiffMerge defaultTP)
+agentKeys=(interval backupPause offsiteControl)
 
-inotifywait -m -q -e close_write,create,delete /datto/config |
+inotifywait -r -m -q -e close_write,create,delete /datto/config |
     while read -r directory events filename; do
-        for key in "${keys[@]}"; do
-            if [ "$filename" = "$key" ]; then
-                echo -n "$filename ($events): "
-                echo -e `cat "$directory$filename"`
-            fi
-        done
+        if [ "$directory" = "/datto/config/" ]; then
+            for key in "${globalKeys[@]}"; do
+                if [ "$filename" = "$key" ]; then
+                    echo -n "$filename ($events): "
+                    echo -e `cat "$directory$filename"`
+                fi
+            done
+        elif [ "$directory" = "/datto/config/keys/" ]; then
+             for key in "${agentKeys[@]}"; do
+                if [ "${filename##*.}" = "$key" ]; then
+                    echo -n "$filename ($events): "
+                    echo -e `cat "$directory$filename"`
+                fi
+            done
+        fi
     done
