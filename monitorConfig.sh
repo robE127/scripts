bd2e555bc88dac23124a8f9a3744e1b08deb7b35
robE127 <roced3@gmail.com>
2016-07-06 10:14:03 -0400

    added more config support

--- monitorConfig.sh
+++ monitorConfig.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
+!/bin/bash
 
 globalKeys=(maxBackups backupOffset useAdvancedAlerting cefActive 
     disableScreenshots useRDPConnection autoDiffMerge defaultTP
     screenshot.queue)
+
 agentKeys=(interval backupPause offsiteControl schedule retention
     offsiteRetention backupEngine screenshotVerification
     screenshotNotification emails include shareCompatibility
     vboxSettings esxSettings writeCache doDiffMerge vssExclude legacyVM
     snapTimeout)
 
-inotifywait -r -m -q -e close_write,create,delete /datto/config |
+homeKeys=(txSpeed speedLimit delay delayStart)
+
+etcKeys=(exports hostname hosts ntp.conf resolv.conf)
+
+inotifywait -m -q --format '%w %e %f' -e attrib,close_write,create,delete,delete_self \
+    /datto/config /datto/config/keys /home/_config /etc/network/interfaces /etc |
     while read -r directory events filename; do
+
         if [ "$directory" = "/datto/config/" ]; then
             for key in "${globalKeys[@]}"; do
                 if [ "$filename" = "$key" ]; then
@@ -18,9 +25,36 @@ inotifywait -r -m -q -e close_write,create,delete /datto/config |
                     echo -e `cat "$directory$filename"`
                 fi
             done
+
         elif [ "$directory" = "/datto/config/keys/" ]; then
              for key in "${agentKeys[@]}"; do
                 if [ "${filename##*.}" = "$key" ]; then
+                    echo -n "$filename ($events): "
+                    echo -e `cat "$directory$filename"`
+                fi
+            done
+
+        elif [ "$directory" = "/home/_config/"  ]; then
+            for key in "${homeKeys[@]}"; do
+                if [ "$filename" = "$key" ]; then
+                    echo -n "$filename ($events): "
+                    echo -e `cat "$directory$filename"`
+                fi
+            done
+
+        elif [ "$directory" = "/datto/config/sync/" ] &&
+                 [ "$filename" = "options" ]; then
+                    echo -n "$filename ($events): "
+                    echo -e `cat "$directory$filename"`
+
+        elif [ "$directory" = "/etc/network/interfaces" ]; then
+                    echo -n "$directory ($events): "
+                    echo -e `cat "$directory"`
+
+        elif [ "$directory" = "/etc/" ]; then
+            for key in "${etcKeys[@]}"; do
+                if [ "$filename" = "$key" ]; then
+                    sleep 1
                     echo -n "$filename ($events): "
                     echo -e `cat "$directory$filename"`
                 fi
